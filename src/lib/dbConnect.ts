4a6ee927a778d62b9b458a800640e6f2876add58
Alok Yadav <yalok6321@gmail.com>
2024-08-29 12:21:24 +0530

    Database connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+	isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnection(): Promise<void> {
+	if (connection.isConnected) {
+		console.log("DB connection already established");
+		return;
+	}
+
+	try {
+		const db = await mongoose.connect(process.env.MONGO_URI || "", {});
+
+        //TODO: check log of db, db.connections
+		connection.isConnected = db.connections[0].readyState;
+		console.log(
+			"DB connection established with " +
+				db.connection.host +
+				":" +
+				db.connection.port
+		);
+	} catch (error) {
+		console.log("Error connecting to DB:", error);
+		process.exit(1);
+	}
+}
+
+export default dbConnection;
