3b82c733dd2a6c3acdce46baa612e3869dfb79c8
Alok Yadav <yalok6321@gmail.com>
2024-08-31 12:39:50 +0530

    Middleware in NextJs

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(request: NextRequest) {
+	const token = await getToken({ req: request });
+	const url = request.nextUrl;
+
+	if (
+		token &&
+		(url.pathname === "/sign-in" ||
+			url.pathname === "/verify" ||
+			url.pathname === "/" ||
+			url.pathname === "/sign-up")
+	) {
+		return NextResponse.redirect(new URL("/dashboard", request.url));
+	}
+
+	if (!token && url.pathname === "/dashboard") {
+		return NextResponse.redirect(new URL("/sign-in", request.url));
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: [
+		"/sign-in",
+		"/sign-up",
+		"/",
+		"/verify/:path*",
+		"/dashboard/:path*",
+	],
+};
+
+export { default } from "next-auth/middleware";
