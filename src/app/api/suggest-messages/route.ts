952613859a44c432b3a144229a7d4e15f9c5bc62
Alok Yadav <yalok6321@gmail.com>
2024-09-01 19:50:25 +0530

    Route to suggest messages

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,21 +1,24 @@
-import { openai } from "@ai-sdk/openai";
 import OpenAi from "openai";
-import { streamText, convertToCoreMessages } from "ai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
+const openai = new OpenAi({
+	apiKey: process.env.OPENAI_API_KEY,
+});
 export const runtime = "edge";
-export const maxDuration = 30;
 
 export async function POST(req: Request) {
 	try {
 		const prompt = `Generate three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are intended for an anonymous social messaging platform similar to Qooh.me. They should be appropriate for a broad audience and should not touch on personal or sensitive subjects. Focus on universal themes that stimulate curiosity and encourage positive interaction. For instance, format the output like this: 'What’s a new hobby you’ve recently picked up?||If you could have dinner with anyone from history, who would it be and why?||What’s a simple pleasure that always brings you joy?'. The questions should be thought-provoking and conducive to friendly and welcoming conversations.`;
 
-		const result = await streamText({
-			model: openai.completion('gpt-3.5-turbo-instruct'),
-            prompt,
-            maxTokens: 120,
+		const response = await openai.completions.create({
+			model: "gpt-3.5-turbo-instruct",
+			max_tokens: 100,
+			stream: true,
+			prompt,
 		});
 
-		return result.toDataStreamResponse();
+		const stream = OpenAIStream(response);
+		return new StreamingTextResponse(stream);
 	} catch (error) {
 		if (error instanceof OpenAi.APIError) {
 			const { name, message, headers, status } = error;
@@ -30,8 +33,9 @@ export async function POST(req: Request) {
 					status,
 				}
 			);
+		} else {
+			console.log("Unexpected error occurred : ", error);
+			throw error;
 		}
-		console.log("Unexpected error occurred : ", error);
-		throw error;
 	}
 }
